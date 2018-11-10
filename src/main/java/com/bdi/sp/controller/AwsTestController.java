d9c9b7ab5ef58f5abbc8e56950871dc3bc117a76
JOCHIHEON <zhixian030@gmail.com>
2018-11-10 17:20:22 +0900

    df

--- src/main/java/com/bdi/sp/controller/AwsTestController.java
+++ src/main/java/com/bdi/sp/controller/AwsTestController.java
@@ -3,12 +3,14 @@ package com.bdi.sp.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.bdi.sp.service.AwsTestService;
 import com.bdi.sp.vo.AwsTestVO;
 
+@Controller
 public class AwsTestController {
 	@Autowired
 	private AwsTestService ats;
